efcdd34e4d4040bcd817b99d60689f1ef2fba813
estrangedwriter <jeromezhang1996@gmail.com>
2020-06-24 18:02:12 -0700

    progress 06/24/2020

--- functions.h
+++ functions.h
@@ -1,8 +1,7 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 char* convert(unsigned int num, int base);
-
 void hexadecconvert(long int decimalNumber);
 void pointeraddress(void *thing);
 int my_putstr(char *str);
--- my_printf.c
+++ my_printf.c
@@ -3,19 +3,13 @@
 #include "functions.h"
 #include <stdlib.h>
 
-
-char* convert(unsigned int num, int base);
-void hexadecconvert(long int decimalNumber);
-void printp(void *thing);
-
 int my_printf(char * restrict parameter, ...) {
-    
     // initializing the variables    
     int index = 0;
-    int i;  
-    char *s; 
+    int sum = 0; 
+    int i;
     int* p;
-    
+    char* s;
     // starting the variadic function
     va_list arg;  // 
     va_start (arg, parameter); //
@@ -27,41 +21,38 @@ int my_printf(char * restrict parameter, ...) {
             my_putchar('\n');
             continue;
         }
-
         while (parameter[index] != '%') {
             my_putchar(parameter[index]);
             index++;
+            sum++;
             if (parameter[index] == '\0') {
                 break;
             }
         }
         if (parameter[index] == '\0') {
-            break;
+           break;
         }
-
         index++;
-        
         // if there is a '%' then some operations must be performed
-
-        switch (parameter[index]) {
-           
+        switch (parameter[index]) {           
             // case 'c' means to print one char at a time 
             case 'c':             
-                i = va_arg(arg, int);
+                i  = va_arg(arg, int);
                 my_putchar(i);
+                sum++;
                 break;
-           
             // case 's' prints a string
             case 's':
                 s = va_arg(arg, char*);
                 int index_s;
                 
-                for (index_s = 0; s[index_s] != '\0'; index_s++)
-                my_putchar(s[index_s]);            
+                for (index_s = 0; s[index_s] != '\0'; index_s++) {
+                    my_putchar(s[index_s]);
+                    sum++;     
+                }
                 break;
-           
             // case 'd' prints a decimal number
-			case 'd' : 
+			case 'd' :
                 i = va_arg(arg, int);
                 if(i<0) { 
                     i = -i;
@@ -69,13 +60,11 @@ int my_printf(char * restrict parameter, ...) {
 				} 
 				my_putstr(convert (i,10));
 			    break; 
-			
             // case 'o' prints an octal number'
 			case 'o': 
                 i = va_arg(arg,unsigned int); //Fetch Octal representation
                 my_putstr( convert (i,8));
-                break; 
-            
+                break;            
             // case 'u' prints an unsigned decimal number
             case 'u':              	
                 i = va_arg(arg, int);
@@ -83,14 +72,12 @@ int my_printf(char * restrict parameter, ...) {
                     i = -i;		 
 				} 
 				my_putstr(convert (i,10));
-			    break; 
-            
+                break; 
             // case 'x' prints a hexadecimal representation
             case 'x': 
                 i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
 				hexadecconvert(i);
-				break; 
-            
+                break;             
             // case 'p' prints a void pointer argument printed in hexadecimal
             case 'p':             
                 p = va_arg(arg, int*);
@@ -98,22 +85,23 @@ int my_printf(char * restrict parameter, ...) {
                 break;
         }
     }
-    return sizeof(parameter);
+    return sum;
 }
 
 int main() {
-    my_printf("Hello world!\n");
-    my_printf("%c!\n", 'H');
-    my_printf("%c%c%c%c%c!\n", 'H', 'e', 'l', 'l', 'o');
-    my_printf("Hello %s!\n", "QWASAR.IO");
-    my_printf("Hello %s %s!\n", "Mr", "Gaetan");
+    printf("%d\n", my_printf("Hello world!\n"));
+    printf("%d\n",my_printf("%c!\n", 'H') );
+    printf("%d\n", my_printf("%c%c%c%c%c!\n", 'H', 'e', 'l', 'l', 'o'));
+    printf("%d\n", my_printf("Hello %s!\n", "QWASAR.IO"));
+    printf("%d\n", my_printf("Hello %s %s!\n", "Mr", "Gaetan"));
     // my_printf("NULL STRING %s!\n", (char*)NULL);
-    my_printf("%d!\n", 1337);
-    my_printf("%d - %d - %d!\n", 2048, 0, -1337);
-    my_printf("%o!\n", 100);
-    my_printf("%u!\n", 1337);
-    my_printf("%x!\n", 14);
-    char *ptr = "cool";
-    my_printf("%p\n", ptr);
+    printf("%d\n", my_printf("%d!\n", 1337));
+    printf("%d\n", my_printf("%d - %d - %d!\n", 2048, 0, -1337));
+    printf("%d\n", my_printf("%o!\n", 100));
+    printf("%d\n", my_printf("%u!\n", 1337));
+    printf("%d\n", my_printf("%x!\n", 14));
+    char* ptr = "cool";
+    char** ptrd = &ptr;
+    printf("%d\n", my_printf("%p\n", ptrd));
     return 0;
 }
